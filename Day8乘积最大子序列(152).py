1b184c8b0254d37ca4cc4a2b2df788309a73d197
TreeCoding <30642264+Bilibotter@users.noreply.github.com>
2020-03-04 10:54:10 +0800

    击败98%

--- Day8乘积最大子序列(152).py
+++ Day8乘积最大子序列(152).py
@@ -1,3 +1,4 @@
+# 只击败85%
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         if len(nums) == 1:
@@ -19,6 +20,32 @@ class Solution:
                     idx = False
             dp.append(pct)
         return maxium
+# 取消dp后，击败98%
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        prev = 1
+        maxium, minium = -float('inf'), -1
+        neg = False
+        for num in nums:
+            pct = num * prev
+            if pct > 0:
+               maxium = max(maxium, pct)
+            elif pct < 0:
+                if neg:
+                    maxium = max(maxium, pct//neg)
+                else:
+                    neg = pct
+            else:
+                maxium = max(maxium, 0)
+                neg = False
+                pct = 1
+            prev = pct
+           
+        return maxium
+            
+
             
 
 
