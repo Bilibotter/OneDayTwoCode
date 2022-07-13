06fa9614f469f396b6c14f4b2c0eeec89e2c22aa
Bilibotter <1102043797@qq.com>
2022-07-13 11:59:14 +0800

    发挥不好，90%

---
+++ 662. 二叉树最大宽度.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        dic = collections.defaultdict(collections.deque)
+        def dfss(i, j ,node):
+            if not node:
+                return
+            dic[i].append(j)
+            if node.left:
+                dfss(i+1, j*2+1, node.left)
+            if node.right:
+                dfss(i+1, j*2+2, node.right)
+        dfss(0, 0, root)
+        maxn = 0
+        for v in dic.values():
+            if len(v) <= 1:
+                maxn = max(len(v), maxn)
+            else:
+                maxn = max(v[-1]-v[0]+1, maxn)
+        return maxn
